349b3562b0e92faebbb6fe4812df31e1defbed5a
unknown <emna.guermazi@stud.enis.tn>
2021-01-22 21:31:12 +0100

    StudentList/studentEdit work from membersList and studentsList

--- src/app/member-edit-form/member-edit-form.component.ts
+++ src/app/member-edit-form/member-edit-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {MemberService} from "../../services/member.service";
-import {StudentFormComponent} from "../member-form/student-form/student-form.component";
-import {StudentModel} from "../../models/student.model";
+import {ActivatedRoute, Router} from '@angular/router';
+import {MemberService} from '../../services/member.service';
+import {StudentFormComponent} from '../member-form/student-form/student-form.component';
+import {StudentModel} from '../../models/student.model';
 
 @Component({
   selector: 'app-member-edit-form',
@@ -10,6 +10,7 @@ import {StudentModel} from "../../models/student.model";
   styleUrls: ['./member-edit-form.component.scss']
 })
 export class MemberEditFormComponent implements OnInit {
+  // tslint:disable-next-line:variable-name
   type_mbr: string;
   currentItemId: any;
   member: any;
@@ -22,24 +23,26 @@ export class MemberEditFormComponent implements OnInit {
     if (!!await this.memberService.getMemberStudentById(this.currentItemId)) {
 
       this.member = await this.memberService.getMemberStudentById(this.currentItemId);
-      console.log(this.member)
-      if(!!this.member.grade){
-        this.member.type_mbr='Enseignant';
-        console.log(this.member.type_mbr)
+      console.log(this.member);
+      if (!!this.member.grade){
+        this.member.type_mbr = 'Enseignant';
+        console.log(this.member.type_mbr);
         this.redirectProfessor();
       }
     else {
-        this.member.type_mbr='Etudiant';
-        console.log(this.member.type_mbr)
+        this.member.type_mbr = 'Etudiant';
+        console.log(this.member.type_mbr);
         this.redirectStudent();
       }
-      console.log(this.member.type_mbr)
+      console.log(this.member.type_mbr);
     }
   }
   redirectStudent(): void {
-    this.router.navigate(['./studentForm'], {queryParams: {id: this.member.id, type_mbr: this.member.type_mbr}}).then(r =>'');
+    this.router.navigate(['./students/student/edit'],
+      {queryParams: {id: this.member.id, type_mbr: this.member.type_mbr}}).then(r => '');
   }
   redirectProfessor(): void {
-    this.router.navigate(['./professorForm'], {queryParams: {id: this.member.id, type_mbr: this.member.type_mbr }}).then(r =>'');
+    this.router.navigate(['./professors/professor/edit'],
+      {queryParams: {id: this.member.id, type_mbr: this.member.type_mbr }}).then(r => '');
   }
 }
